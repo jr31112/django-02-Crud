f3ae799aab3b32ff224ebe6a66781a7b627e759b
jr31112 <jr435@naver.com>
2019-08-19 16:40:20 +0900

    04 | articles -CRUD - C

--- articles/views.py
+++ articles/views.py
@@ -9,3 +9,19 @@ def index(request):
 
     }
     return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    # 받아오는 로직
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    # 저장하는 로직
+    article = Article(title = title, content = content)
+    article.save()
+    # 전달하는 로직
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/create.html', context)
