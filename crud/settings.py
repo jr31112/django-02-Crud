82da984831617ee63a39b62bf67e193fb1414f07
jr31112 <jr435@naver.com>
2019-09-23 15:45:34 +0900

    23 | Add thumbnail with django-imagekit

--- crud/settings.py
+++ crud/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'articles',
     'bootstrap4',
+    'imagekit',
     'django.contrib.admin', # admin
     'django.contrib.auth',
     'django.contrib.contenttypes',
