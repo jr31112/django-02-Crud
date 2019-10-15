a0735d052e2f266950ab32d938a9be12fddc5b58
jr31112 <jr435@naver.com>
2019-10-15 14:36:04 +0900

    26,27 | Account login logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -14,3 +17,21 @@ def signup(request):
         'form' : form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
